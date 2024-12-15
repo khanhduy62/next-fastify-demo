c785c9c23a640217faf2f26669e30784b0d89b79
Duy Le <lekhanhduyduy1201@gmail.com>
2024-12-15 13:29:10 +0700

    change description

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
-import './globals.css'
-import { Toaster } from '@/components/ui/toaster'
-import AppProvider from '@/app/app-provider'
-import SlideSession from '@/components/slide-session'
-import { baseOpenGraph } from '@/app/shared-metadata'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import AppProvider from "@/app/app-provider";
+import SlideSession from "@/components/slide-session";
+import { baseOpenGraph } from "@/app/shared-metadata";
 // import dynamic from 'next/dynamic'
-import Header from '@/components/header'
+import Header from "@/components/header";
 // const Header = dynamic(() => import('@/components/header'), { ssr: false })
-const inter = Inter({ subsets: ['vietnamese'] })
+const inter = Inter({ subsets: ["vietnamese"] });
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Productic',
-    default: 'Productic'
+    template: "%s | Productic",
+    default: "Productic",
   },
-  description: 'Được tạo bởi Được dev',
-  openGraph: baseOpenGraph
-}
+  description: "Được tạo bởi Duy Le",
+  openGraph: baseOpenGraph,
+};
 
 export default async function RootLayout({
-  children
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
         <Toaster />
         <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
+          attribute="class"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
@@ -43,5 +43,5 @@ export default async function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
